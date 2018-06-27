13fd015998ef85e23e9e16d70a3513cb59daf59f
hjnp <hp_ctt@hotmail.com>
2018-06-27 17:52:30 +0100

    Add new assert fail test

--- v1/test/2_initial_tests.js
+++ v1/test/2_initial_tests.js
@@ -11,22 +11,23 @@ contract("| Storage and upgradability example | ", async accounts => {
     
     let proxyContract;
     
-    it("deploy version 1 of contract", async () => {
-        
+    it("Deploy version 1 of contract", async () => {
         const contractLogicV1 = await ContractLogicV1.new();
-
+        
         proxyContract = await Proxy.new(contractLogicV1.address)
         
+        //TODO: check other ways to call ContractLogic without _.extend
+
         // proxyContract = ContractLogicV1.at(proxyContract.address);
         proxyContract = _.extend(proxyContract, ContractLogicV1.at(proxyContract.address));
         
         await proxyContract.setVersion(1);
         const version = await proxyContract.getVersionNumber();
         
-        assert.equal(version, 1, "version 1 is set");
+        assert.equal(version.toNumber(), 1, "version 1 is set");
     });
     
-    it("deploy version 2 of contract", async () => {
+    it("Deploy version 2 of contract", async () => {
         const contractLogicV2 = await ContractLogicV2.new();
         const receiptUpgrade = await proxyContract.upgradeTo(contractLogicV2.address);
 
@@ -38,9 +39,35 @@ contract("| Storage and upgradability example | ", async accounts => {
 
         await proxyContract.setV(2);
         const version = await proxyContract.getV();
-        console.log('B: ​version:', version.toNumber());
 
-        assert.equal(version, 2, "version 2 is set");
+        assert.equal(version.toNumber(), 2, "version 2 is set");
+    });
+
+    it("Check if other account can change version of implementation", async () => {
+        
+        const contractLogicV2 = await ContractLogicV2.new();
+        const notOwnerAccount = accounts[2];
+        
+        let receiptUpgrade;
+        
+        try {
+            receiptUpgrade = await proxyContract.upgradeTo(contractLogicV2.address, { from: notOwnerAccount });
+        } catch(error) {
+            const invalidOwnerAccount = error.message.indexOf('revert' >= 0);
+            // TODO: When we contract A calls contract B, and B throws, instead
+            //       of an 'invalid jump', we get an 'out of gas' error. How do
+            //       we distinguish this from an actual out of gas event? (The
+            //       testrpc log actually show an 'invalid jump' event.)
+            const outOfGas = error.message.search('out of gas') >= 0;
+
+            assert(
+                invalidOwnerAccount || outOfGas,
+                "Expected throw, got '" + error + "' instead",
+              );
+            return;
+        };
+        
+        assert.fail('Expected throw not received');
     });
 
 });
