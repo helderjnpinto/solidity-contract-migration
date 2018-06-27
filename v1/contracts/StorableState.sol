327998668d0221e6da69f1074b7066d77078a571
hjnp <hp_ctt@hotmail.com>
2018-06-27 15:31:20 +0100

    fix contract

--- v1/contracts/StorableState.sol
+++ v1/contracts/StorableState.sol
@@ -3,5 +3,9 @@ pragma solidity ^0.4.24;
 import "./KeyStore.sol";
 
 contract StorableState {
-  KeyStore _state;
+  KeyStore _state = new KeyStore();
+  constructor() {
+
+  }
+  
 }
--- v1/migrations/2_initial_migration_shared_storage.js
+++ v1/migrations/2_initial_migration_shared_storage.js
@@ -3,10 +3,10 @@ var cv1 = artifacts.require("./migrations/v1.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(cv1).then(function(cv1Instance) {
-  console.log('​module.exports -> cv1Instance', cv1Instance.address);
+  console.log('​|||||| cv1Instance', cv1Instance.address);
     
     return deployer.deploy(Proxy, cv1Instance.address).then(function(ProxyInstance) {
-    console.log('​module.exports -> ProxyInstance', ProxyInstance.address);
+    console.log('|||||| ProxyInstance', ProxyInstance.address);
 
     });
   });
